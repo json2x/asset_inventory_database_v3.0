e14c402b02d4653b564d2e600874678ec1c44279
JCM <jcmillan@smart.com.ph>
2021-09-01 14:39:16 +0800

    Merged edrar app with form logic using js, and nmsdata app with its models

--- edrar/serializers.py
+++ edrar/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from api.serializers import DevicesSerializer, TrxSerializer
-from api.models import Device, Cell, Trx
+from api.models import Cell
 
 class CellsDeviceTrxSerializer(serializers.ModelSerializer):
 
@@ -14,38 +14,4 @@ class CellsDeviceTrxSerializer(serializers.ModelSerializer):
             'tech', 'band', 'admin_state', 'alias', 'lac_tac', 'sac_ci_eutra', 'rnc_cid', 'phy_cid', 
             'lcr_cid', 'mcc', 'mnc', 'nodeid', 'sector_id', 'carrier', 'ne_type', 'subdomain', 'function', 
             'sdcch_cap', 'tch_cap', 'azimuth', 'record_status', 'trx', 'device'
-        )
-
-class NmsDevicesSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Device
-        fields = (
-            'id', 'dn', 'device_id', 'ems_device_id', 'device_alias', 'device_ip', 'ems_id', 'vendor_id', 
-            'ne_type', 'model', 'hardware_description', 'functional_description', 'parent_device_id', 
-            'parentdn', 'site_id', 'device_state', 'software_version', 'integration_date', 'end_of_support', 
-            'tsa_scope', 'product_id', 'serial_number', 'freq_tx_rx_field', 'hardware_capacity', 'domain', 
-            'ne_owner', 'tx_clusterimg', 'tx_type', 'natspcode', 'admin_state', 'subdomain', 'function', 
-            'iubce_dl_lic', 'iubce_ul_lic', 's1cu_lic', 'cluster_region', 'cluster_sub_region', 
-            'cluster_province', 'cluster_city', 'mw_hub', 'record_status'
-        )
-
-class NmsCellsSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Cell
-        fields = (
-            'id', 'domain', 'ems_cell_id', 'ems_id', 'cell_name', 'dn', 'site', 'parent_id', 'parent_dn', 
-            'tech', 'band', 'admin_state', 'alias', 'lac_tac', 'sac_ci_eutra', 'rnc_cid', 'phy_cid', 
-            'lcr_cid', 'mcc', 'mnc', 'nodeid', 'sector_id', 'carrier', 'ne_type', 'subdomain', 'function', 
-            'sdcch_cap', 'tch_cap', 'azimuth', 'record_status'
-        )
-    
-class NmsTrxSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Trx
-        fields = (
-            'id', 'ems_trx_id', 'ems_id', 'trx_name', 'dn', 'site_id', 'parent_id', 
-            'parent_dn', 'admin_state', 'e1_assignment', 'homing_bts', 'record_status'
         )
